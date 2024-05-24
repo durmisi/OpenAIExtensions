d7af0fd952e647c3137178b30f84d148356cbad5
Admir Durmishi <durmisi@gmail.com>
2024-05-24 20:50:45 +0200

    Added AITextService and Brainstorm on user idea function

---
+++ Core/Services/AITextService.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+
+namespace OpenAIExtensions.Services
+{
+    public interface IAITextService
+    {
+        public Task<string> Brainstorm(string input, CancellationToken ct = default);
+    }
+
+    public class AITextService : IAITextService
+    {
+        private readonly Kernel _kernel;
+        private readonly ILogger<AITextService> _logger;
+
+        public AITextService(
+            Kernel kernel,
+            ILogger<AITextService> logger)
+        {
+            _kernel = kernel;
+            _logger = logger;
+        }
+
+        public async Task<string> Brainstorm(string input, CancellationToken ct = default)
+        {
+
+            string prompt = $@"
+                You are a helpful assistant.
+                I have an idea I'd like to brainstorm around.
+    
+                Here it is: {input}
+    
+                It's a [brief description of the idea or its purpose].
+    
+                Can you provide various aspects such as potential challenges, opportunities, innovative angles, and more? I'm looking for detailed suggestions from different perspectives.
+            ";
+
+            var chatCompletionService = _kernel.GetRequiredService<IChatCompletionService>();
+
+            var chatHistory = new ChatHistory();
+
+            chatHistory.AddUserMessage(prompt);
+
+            var response = await chatCompletionService
+                .GetChatMessageContentAsync(chatHistory: chatHistory,
+                executionSettings: new OpenAIPromptExecutionSettings()
+                {
+                    MaxTokens = 1000,
+                    Temperature = 0.5f,
+                    FrequencyPenalty = 0.5f,
+                    PresencePenalty = 0.5f,
+                    TopP = 0.9f // Top P
+                },
+                kernel: _kernel,
+                cancellationToken: ct);
+
+            string responseMessage = response.Content ?? "";
+
+            return responseMessage;
+        }
+
+    }
+}
---
+++ OpenAIExtensions.Tests/AITextServiceTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using OpenAIExtensions.Services;
+using Xunit.Abstractions;
+
+namespace OpenAIExtensions.Tests;
+
+public class AITextServiceTests : IntegrationTestBase
+{
+    private readonly AITextService _translationService;
+
+    public AITextServiceTests(ITestOutputHelper outputHelper)
+        : base(outputHelper)
+    {
+        var logger = CreateLogger<AITextService>();
+
+        var endpoint = Configuration.GetValue<string>("OpenAI:Endpoint")!;
+        var key = Configuration.GetValue<string>("OpenAI:Key")!;
+
+        var kernel = SematicKernelBuilder.Create()
+            .AddAIChatCompletion(endpoint: endpoint, apiKey: key)
+            .Build();
+
+        _translationService = new AITextService(kernel, logger);
+    }
+
+    [Fact]
+    public async Task AITextService_Brainstorm_generates_an_idea()
+    {
+        //Arrange
+        var userIdea = @"
+
+            I have this concept for a mobile app that helps people track their daily water intake and reminds them to stay hydrated throughout the day. 
+            Can you brainstorm various aspects of this idea, like potential challenges, opportunities for growth, innovative features, 
+            and how to differentiate it from existing hydration apps? 
+            I'm eager to explore this idea from different angles and get detailed suggestions.
+            ";
+
+
+        //Act
+        var answer = await _translationService.Brainstorm(userIdea);
+
+        //Assert
+        Assert.NotNull(answer);
+        Assert.NotEmpty(answer);
+
+        WriteToConsole(userIdea);
+        WriteToConsole(answer);
+    }
+
+}
